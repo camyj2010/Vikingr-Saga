cd67b4894ba84656041542baf9f6d2ce7b82b494
Santiago Gutierrez Hincapie <gutierrez.santiago@correounivalle.edu.co>
2023-05-27 01:28:32 -0500

    updates

--- client/src/pages/Map/Icon.jsx
+++ client/src/pages/Map/Icon.jsx
@@ -6,33 +6,27 @@ import * as THREE from 'three'
 import { DoubleSide } from 'three';
 import { useNavigate } from 'react-router-dom';
 import Swal from 'sweetalert2/dist/sweetalert2.js'
+import Island1 from '../Island1/Island1';
 
 export default function introLecture() {
     const colorMap = useLoader(TextureLoader, icon)
     const coin = useRef(null)
-    const navigate = useNavigate(   )
-
+    const navigate = useNavigate()
     
 
     useFrame((state, delta)=>{
         if(coin.current.position.y < 45){
-            coin.current.position.y += 2 * delta   
-            coin.current.rotation.x += 3 * delta          
+            coin.current.position.y += 8 * delta   
+            coin.current.rotation.z += 8 * delta          
         }
-        // else{
-        //     coin.current.position.y -= 2 * delta   
-        //     coin.current.rotation.x -= 4 * delta 
-        // }
-
+        
         if(coin.current.rotation.x > (- Math.PI * 0.5)){
-            coin.current.rotation.x += 4 * delta
-            // coin.current.rotation.y += 4 * delta  
-            //console.log(coin.current.rotation.y) 
+            coin.current.rotation.z += 1 * delta
         }
     })
 
     return (
-        <mesh ref={coin} scale={8} rotation={[- Math.PI * 0.5,  Math.PI * 0.5, 0] } position={[70, 25, 50]} onClick={() => navigate('/Leccion1')}
+        <mesh ref={coin} scale={8} rotation={[Math.PI * 0.5,  0, 0] } position={[70, 25, 50]} onClick={() => navigate('/Leccion1')}
     >
             <cylinderGeometry args={[1, 1, 0.1, 64, 1]} />
             <meshStandardMaterial side={DoubleSide} map={colorMap}/>
