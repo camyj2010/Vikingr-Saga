4fde32bd28d8f5f7ae9fb349dbe38e814ac4d5de
Sebastian <roncancio.juan@correounivalle.edu.co>
2023-05-27 14:05:50 -0500

    eleventh try, i lost all my faith

--- client/src/jestTesting.test.js
+++ client/src/jestTesting.test.js
@@ -1,13 +1,14 @@
 import { sign_in, sign_up } from './pages/api/Handleapi.js'
 
-global.fetch = jest.fn(() =>
+
+
+test('El usuario puede iniciar sesión correctamente', async () => {
+    global.fetch = jest.fn(() =>
   Promise.resolve({
     json: () => Promise.resolve({ acceso: 'access_token' }),
     status: 200
   })
 );
-
-test('El usuario puede iniciar sesión correctamente', async () => {
     const email = 'atlasAdmin@gmail.com';
     const password = '123';
     const result = await sign_in(email, password);
@@ -15,14 +16,15 @@ test('El usuario puede iniciar sesión correctamente', async () => {
     expect(result).toBe('access_token');
   });
 
-  global.fetch = jest.fn(() =>
+  
+
+test('El usuario se puede registrar correctamente', async () => {
+    global.fetch = jest.fn(() =>
   Promise.resolve({
     json: () => Promise.resolve({ ok: true }),
     status: 200
-  })
+  })    
 );
-
-test('El usuario se puede registrar correctamente', async () => {
   const nickname = 'testuser';
   const email = 'test@gmail.com';
   const password = '12345';
