19f11767da0a9c2d01f6f563a2958a99ff479852
camyj2010 <maria.jaramillo.andrade@correounivalle.edu.co>
2023-06-20 16:10:14 -0500

    new backend funcctions ans endpoints

--- client/src/pages/Hut.jsx
+++ client/src/pages/Hut.jsx
@@ -1,4 +1,4 @@
-import {React, useRef} from 'react'
+import {React, useRef, useContext,useEffect} from 'react'
 import { Canvas } from '@react-three/fiber'
 import { Suspense } from 'react'
 import Experience from './Map/Experience'
@@ -6,17 +6,34 @@ import  {Loader} from '@react-three/drei'
 import './styles/Hut.css'
 import map from '../img/mapa-del-tesoro.png';
 import hut from '../img/mini_hut.png';
+import { useUserContext } from './UserProvider';
+import { getUserInfo } from './api/Handleapi';
 
 
 export default function Hut() {
-      
-
+    const { user } = useUserContext();
+  
+    const fetchUserInfo = async () => {
+      try {
+        if (user !== null) {
+          const userInfo = await getUserInfo(String(user));
+          console.log(userInfo);
+          // Realiza las operaciones necesarias con la información del usuario obtenida
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+  
+    useEffect(() => {
+      fetchUserInfo();
+    }, [user]);
+  
     return (
-        <div className='hut_ambient'>
-            <a href='/Home'>
-                <img className='map_image_hut' src={map} />
-            </a>
-           
-        </div>
-    )
-}
+      <div className='hut_ambient'>
+        <a href='/Home'>
+          <img className='map_image_hut' src={map} alt='Map' />
+        </a>
+      </div>
+    );
+  }
